500cc7b0a74ae5ce171625fce247c66046c9ba78
lucian606 <iliesculucian1508@gmail.coom>
2025-02-25 00:02:01 +0200

    Add unit tests for ExpirationDiscountStrategy

--- src/main/kotlin/org/example/onlinestoreapi/strategies/ExpirationDiscountStrategy.kt
+++ src/main/kotlin/org/example/onlinestoreapi/strategies/ExpirationDiscountStrategy.kt
@@ -3,19 +3,24 @@ package org.example.onlinestoreapi.strategies
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 import org.example.onlinestoreapi.entities.Product
-import org.springframework.beans.factory.annotation.Autowired
 import java.time.Clock
 
 class ExpirationDiscountStrategy(val expirationLimit: Long) : DiscountStrategy {
-    @Autowired
+
     private var clock: Clock = Clock.systemUTC()
+
+    constructor(
+        expirationLimit: Long,
+        clock: Clock
+    ) : this(expirationLimit) {
+        this.clock = clock
+    }
 
     override fun applyDiscount(product: Product, discount: Int): Product {
         val currentDate = LocalDate.now(clock)
-        println(currentDate)
         val daysUntilExpiration = ChronoUnit.DAYS.between(currentDate, product.expirationDate)
 
-        return if (daysUntilExpiration < expirationLimit) {
+        return if (daysUntilExpiration in 0..expirationLimit) {
             product.copy(price = product.price * (100f - discount) / 100)
         } else {
             product
--- src/test/kotlin/org/example/onlinestoreapi/CategoryDiscountStrategyTest.kt
+++ src/test/kotlin/org/example/onlinestoreapi/CategoryDiscountStrategyTest.kt
@@ -1,25 +1,22 @@
 package org.example.onlinestoreapi
 
-import io.mockk.junit5.MockKExtension
 import org.example.onlinestoreapi.entities.Product
 import org.example.onlinestoreapi.entities.ProductCategory
 import org.example.onlinestoreapi.strategies.CategoryDiscountStrategy
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import java.time.LocalDate
 import java.util.*
 import kotlin.test.assertEquals
 
-@ExtendWith(MockKExtension::class)
 class CategoryDiscountStrategyTest {
     @Test
     fun `should apply discount to products with discounted categories`() {
         val discount = 10
         val product = Product(UUID.randomUUID(), "name", ProductCategory.MEAT, LocalDate.now(), 100.0)
-        val expectedProduct = product.copy(price = product.price * (100f - discount) / 100)
         val discountedCategories = setOf(ProductCategory.MEAT, ProductCategory.CHEESE)
 
         val strategy = CategoryDiscountStrategy(discountedCategories)
+        val expectedProduct = product.copy(price = product.price * (100f - discount) / 100)
         assertEquals(expectedProduct, strategy.applyDiscount(product, discount))
     }
 
