06f7291392cf53058fe2e04bf6c02517582b5af1
lucian606 <iliesculucian1508@gmail.coom>
2025-06-18 21:19:10 +0300

    Move test resources folder

--- src/test/kotlin/org/example/onlinestoreapi/e2e/ApplicationTests.kt
+++ src/test/kotlin/org/example/onlinestoreapi/e2e/ApplicationTests.kt
@@ -11,7 +11,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
-import org.springframework.http.ResponseEntity
 import org.springframework.test.annotation.DirtiesContext
 import java.time.Clock
 import java.time.Instant
